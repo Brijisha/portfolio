b6bdf4e907f959a23fa4fc4b0a24b0cfafd9c5b2
Brijisha Doshi <brijishadoshi12@gmail.com>
2022-08-25 22:28:43 +0530

    Changes in About page

--- src/components/About.js
+++ src/components/About.js
@@ -8,13 +8,13 @@ function About() {
         Hey 👋
       </p>
 
-      <p className="text-base text-gray-800 md:text-xl lg:text-2xl text-center mt-3 font-serif">
-        I'm a passionate Web and React Developer. I love creating websites. I
-        also love to do frelancing projects.I am very fond of learning new
-        things. I dvelope responsive website using React.js and Tailwind CSS.
-        I'm also learning Back-End Development and looking forward to be a Full
-        Stack Web Developer. I have created REST API in Node.js uning Express
-        server. I have done my Bachelor of Engineering in Computer Science.
+      <p className="text-base md:text-xl lg:text-2xl text-center mt-3 font-serif text-black font-medium">
+        I have done my Bachelor of Engineering in Computer Science. I'm a
+        passionate Web and React Js Developer. I love creating websites. I also
+        love to do frelancing projects. I am very fond of learning new things. I
+        develope responsive website using React.js and Tailwind CSS. I'm also
+        learning Back-End Development and looking forward to be a Full Stack Web
+        Developer. I have created REST API in Node.js uning Express server.
       </p>
       <ScrollIntoView selector="#tech">
         <div className="mx-auto p-5">
